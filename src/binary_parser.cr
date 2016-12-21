a941a7f5e97a55174b28d72202557f44acdd3feb
DanSnow <dododavid006@gmail.com>
2016-12-21 13:30:43 +0800

    refactor: Move ByteSize module into BinaryParser

--- src/binary_parser.cr
+++ src/binary_parser.cr
@@ -259,31 +259,32 @@ class BinaryParser
       {% end %}
     end
   end
-end
 
-module ByteSize
-  macro included
-    @static_size : Int32?
+  module ByteSize
+    macro included
+      @static_size : Int32?
 
-    def bytesize
-      dyn_size = 0
-      {% for func in @type.methods %}
-        {% if func.name.starts_with? "_size_dyn" %}
-          dyn_size += {{func.name}}
+      def bytesize
+        dyn_size = 0
+        {% for func in @type.methods %}
+          {% if func.name.starts_with? "_size_dyn" %}
+            dyn_size += {{func.name}}
+          {% end %}
         {% end %}
-      {% end %}
-      static_size + dyn_size
-    end
+        static_size + dyn_size
+      end
 
-    private def static_size
-      return @static_size.not_nil! if @static_size
-      size = 0
-      {% for func in @type.methods %}
-        {% if func.name.starts_with? "_size_static" %}
-          size += {{func.name}}
+      private def static_size
+        return @static_size.not_nil! if @static_size
+        size = 0
+        {% for func in @type.methods %}
+          {% if func.name.starts_with? "_size_static" %}
+            size += {{func.name}}
+          {% end %}
         {% end %}
-      {% end %}
-      @static_size = size
+        @static_size = size
+      end
     end
   end
 end
+
